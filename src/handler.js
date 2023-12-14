f09f70db51a8884b862b080431bdb02e796ef7df
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2023-12-14 15:30:30 +0800

    feat: add put book handler

--- src/handler.js
+++ src/handler.js
@@ -115,4 +115,85 @@ const getBookDetailByIdHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addBookHandler, getBooksHandler, getBookDetailByIdHandler };
+const putBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+
+    response.code(400);
+    return response;
+  }
+
+  const bookDetail = books.filter((book) => book.id === bookId);
+  if (!bookDetail.length) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+  }
+
+  const index = books.findIndex((book) => book.id === bookId);
+  let finished = false;
+  if (readPage === pageCount) {
+    finished = true;
+  }
+
+  const updatedAt = new Date().toISOString();
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      finished,
+      reading,
+      updatedAt,
+    };
+  }
+
+  const response = h.response({
+    status: 'success',
+    message: 'Buku berhasil diperbarui',
+  });
+
+  response.code(200);
+  return response;
+};
+
+module.exports = {
+  addBookHandler,
+  getBooksHandler,
+  getBookDetailByIdHandler,
+  putBookByIdHandler,
+};
