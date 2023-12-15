e31490a9fe1751d29ebee6aeebc10632fa32b801
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2023-12-15 19:05:28 +0800

    feat: add delete book handler

--- src/handler.js
+++ src/handler.js
@@ -191,9 +191,35 @@ const putBookByIdHandler = (request, h) => {
   return response;
 };
 
+const deleteBookHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    books.splice(index);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. id tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getBooksHandler,
   getBookDetailByIdHandler,
   putBookByIdHandler,
+  deleteBookHandler,
 };
